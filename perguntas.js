db435039b6134aec8921beeccdec41fc28167898
santosyummi <141366425+santosyummi@users.noreply.github.com>
2024-10-22 09:01:58 -0300

    ok

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
     'Programação',
     'O que é html?',
-    'É uma Linguagem de Marcação de HiperTexto',
+    'É uma Linguagem de Marcação de HiperTexto.',
 )
 
 criaCartao(
     'Programação',
     'O que é Java Script?',
-    'É uma linguagem de programação que permite colocar itens em páginas web'
+    'É uma linguagem de programação que permite colocar itens em páginas web.'
 )
 
 criaCartao(
     'Programação',
     'O que o css faz?',
-    'Permite a personalização da aparência dos elementos HTML'
+    'Permite a personalização da aparência dos elementos HTML.'
 )
 
 criaCartao(
     'Programação',
     'O que é o body?',
-    'É o documento em si, a informação legível para o leitor do documento'
+    'É o documento em si, a informação legível para o leitor do documento.'
 )
 
 criaCartao(
     'Programação',
-    'O que é o body?',
-    'É o documento em si, a informação legível para o leitor do documento'
+    'O que é o header?',
+    'Conhecido como “cabeçalho”, É a parte superior do site, onde fica destacada do resto do conteúdo.'
 )
 
 criaCartao(
     'Programação',
-    'O que é o body?',
-    'É o documento em si, a informação legível para o leitor do documento'
+    'O que é o footer?',
+    'É o elemento de “fechamento” de conteúdo de página.'
 )
